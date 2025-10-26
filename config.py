e39dd5e6e5b1bbcdf477f67d618f4401a79fe751
rohit37816-ui <rohit37816@gmail.com>
2025-10-26 13:08:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 LOGIN_SYSTEM = os.environ.get('LOGIN_SYSTEM', 'True').lower() == 'true'
 
 # Always define STRING_SESSION so the import never fails
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOIwBu0eJzjYXiIZFT855fYLU1r7idwoQIp8wDtTcuGCd1EsqznDKk1baqly0aMKUtCR7nO6DMFLXqJi7fgrLtrvdcfoQ8I5eNCQLEIkmcWkozpvT3ZxjcDDoZnDLdPrIF6L_Q3Y_VvekZWd8YWYgunMcKb-9B9oTVxbpCPt-EnGp1EirTQ4y2OPtWxumv2XMN0VK1AXhnZ7Gtxdr3n4mToG-NXS07ilaCBFeRhT7UWka1-lJy59iZiDzqzMqyJkCip_nDMKTbouTunqjym8U60_a8Qm_YqQ5V_zHQnqzYF3WX1YLVEGisiyRegJ5XDucdOYJ7M7UP0bFgY1JJPdwsFHJsMI=")
 
 # --- TELEGRAM BOT CREDENTIALS ---
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8479055966:AAEgmGioIw9EHhO0Xo2E_97hOSMMmwY9D0w")
